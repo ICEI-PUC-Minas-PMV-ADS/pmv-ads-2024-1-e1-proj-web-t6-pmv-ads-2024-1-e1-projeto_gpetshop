b4a189c0ab29175e7de9df34c39e19f0082d7b10
Paulo H <contacthenry96@gmail.com>
2024-05-06 16:20:46 -0300

    Carregado a node_modules

--- codigo-fonte/webpack.config.js
+++ codigo-fonte/webpack.config.js
@@ -2,37 +2,37 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: './src/index.js',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/index.js',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+        historyApiFallback: true,
     },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader',],
-      },
-      {
-        test: /\.svg$/,
-        use: ['file-loader'],
-      }
-    ]
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: "index.html",
-    }),
-  ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader',],
+            },
+            {
+                test: /\.svg$/,
+                use: ['file-loader'],
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: "index.html",
+        }),
+    ],
 }
