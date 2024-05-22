f339219692f287bf0fec1c0dc1525aabec55d49f
Rafael Santos Carvalho <88905368+Oieusouopi@users.noreply.github.com>
2024-05-22 17:51:23 -0300

    Update index.js

--- codigo-fonte/src/index.js
+++ codigo-fonte/src/index.js
@@ -1,81 +1,14 @@
-import { Calendar } from '@fullcalendar/core';
-import interactionPlugin from '@fullcalendar/interaction';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
-import './index.css';
-
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    initialDate: '2018-01-12',
-    navLinks: true, // can click day/week names to navigate views
-    editable: true,
-    dayMaxEvents: true, // allow "more" link when too many events
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2018-01-01',
-      },
-      {
-        title: 'Long Event',
-        start: '2018-01-07',
-        end: '2018-01-10'
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2018-01-09T16:00:00'
-      },
-      {
-        groupId: 999,
-        title: 'Repeating Event',
-        start: '2018-01-16T16:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2018-01-11',
-        end: '2018-01-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2018-01-12T10:30:00',
-        end: '2018-01-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2018-01-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2018-01-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2018-01-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2018-01-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2018-01-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2018-01-28'
-      }
-    ]
-  });
-
-  calendar.render();
-});
+import "./index.css";
+import depoimentos1 from  "../Imagens/depoimentos1 (1).png";
+import depoimentos2 from  "../Imagens/depoimentos1 (2).png";
+import depoimentos3 from  "../Imagens/depoimentos1 (3).png";
+import depoimentos4 from  "../Imagens/depoimentos1 (4).png";
+import depoimentos5 from  "../Imagens/depoimentos1 (5).png";
+import depoimentos6 from  "../Imagens/depoimentos1 (6).png";
+import icon_facebook from  "../Imagens/icon_facebook.png";
+import icon_instagram from  "../Imagens/icon_instagram.png";
+import icon_whatsapp from  "../Imagens/icon_whatsapp.png";
+import imagem_quem_somos from  "../Imagens/ImagemQuemSomos.png";
+import logo_pet_puc from  "../Imagens/LogoPetpuc.png";
+import servico_banho from  "../Imagens/servicoBanho.png";
+import servico_tosa from  "../Imagens/servicoTosa.png";
