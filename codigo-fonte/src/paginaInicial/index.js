46d1f3c0c6a61dda60f2e09843a026ad1c315018
joaohleite <jhaleite@sga.pucminas.br>
2024-05-24 11:14:23 -0300

    Alteração nos códigos Index HTML, CSS e JS

--- codigo-fonte/src/paginaInicial/index.js
+++ codigo-fonte/src/paginaInicial/index.js
@@ -1,3 +1,43 @@
-import "./index.css"
+let lista = localStorage.getItem("minhaLista")
+const formulario = document.querySelector("form");
 
-console.log('hello world')
+if (lista) {
+    lista = JSON.parse(lista);
+} else {
+    lista = []
+}
+
+formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let novaPessoa = new Object();
+    novaPessoa.nome = this.nome.value;
+    novaPessoa.email = this.email.value;
+    novaPessoa.telefone = this.telefone.value;
+    novaPessoa.mensagem = this.mensagem.value;
+
+    lista.push(novaPessoa);
+
+    lista.forEach((item, index) => {
+        item.id = index + 1;
+    })
+
+    this.reset();
+
+    localStorage.setItem("minhaLista", JSON.stringify(lista));
+
+
+});
+
+function formatarTelefone(input) {
+    var telefone = input.value.replace(/\D/g, '');
+
+    if(telefone[2]== 9) {
+        telefone = telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3');
+    } else {
+        telefone = telefone.replace(/(\d{2})(\d{4})(\d{4})/, '($1)$2-$3');
+    }
+
+    input.value = telefone
+}
+
+
