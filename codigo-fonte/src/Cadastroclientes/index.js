4d606ced4ac546b91909cbd29747b296ae541185
felipe12M <felipe.costa1@hotmail.com>
2024-05-31 12:05:02 -0300

    correção nos códigos Index HTML, CSS e JS

--- codigo-fonte/src/Cadastroclientes/index.js
+++ codigo-fonte/src/Cadastroclientes/index.js
@@ -1,7 +1,26 @@
-import './index.css';
-import '../Imagens/agenda.png';
-import '../Imagens/cadastro.png';
-import '../Imagens/file.png';
-import '../Imagens/home.png';
-import '../Imagens/power.png';
-import '../Imagens/suporte.png';
+document.addEventListener("DOMContentLoaded", function () {
+  const addInputButtons = document.querySelectorAll(".plus-pet-icon");
+
+  addInputButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const divPetAdd = document.getElementById("div-pet-add");
+      const newDivPetAdd = divPetAdd.cloneNode(true);
+
+      const plusPetIcon = newDivPetAdd.querySelector(".plus-pet-icon");
+      if (plusPetIcon) {
+        plusPetIcon.remove();
+      }
+
+      newDivPetAdd
+        .querySelectorAll('input[type="text"]')
+        .forEach(function (input) {
+          input.value = "";
+        });
+
+      divPetAdd.parentNode.insertBefore(newDivPetAdd, divPetAdd.nextSibling);
+    });
+  });
+});
+ 
