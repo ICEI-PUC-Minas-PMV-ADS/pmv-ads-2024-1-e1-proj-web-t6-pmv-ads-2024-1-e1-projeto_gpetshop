fefdafa99cb5d30fb4a0ef205d1048fc28f7370e
paulo-h-carvalho <paulo.carvalho.1524995@sga.pucminas.br>
2024-06-07 00:46:09 -0300

    Ajustes tela novo agendamento

--- codigo-fonte/src/NovoAgendamento/index.js
+++ codigo-fonte/src/NovoAgendamento/index.js
@@ -12,52 +12,74 @@ const clearFields = () => {
   dataClient.forEach((field) => (field.value = ""));
 };
 
-const sendRequest = () => {
-  if (isValidFields()) {
-    const db_client = getLocalStorage();
-    const newClientId = db_client.length + 1; // Gera o ID baseado na posição no localStorage
+function sendClient() {
+  const db_client = getLocalStorage();
+  const newClientId = db_client.length > 0 ? db_client[db_client.length - 1].id + 1 : 1; // Gera o ID de forma incremental
 
-    const clientRequest = {
-      cliente: {
-        id: newClientId, // Usa o ID gerado
-        nome: document.getElementById("nameClient").value,
-        email: document.getElementById("emailClient").value,
-        celular: document.getElementById("foneClient").value,
-      },
+  const id = newClientId; // Usa o ID gerado
+  const nome = document.getElementById("nameClient").value.trim();
+  const email = document.getElementById("emailClient").value.trim();
+  const celular = document.getElementById("foneClient").value.trim();
+  const namePet = document.getElementById("namePet").value.trim();
+  const idade = document.getElementById("idade").value.trim();
+  const porte = document.getElementById("porte").value.trim();
+  const pelagem = document.getElementById("pelagem").value.trim();
+  const raca = document.getElementById("raca").value.trim();
+  const services = document.getElementById("services").value.trim();
+  const agenda = document.getElementById("dt-agenda").value.trim();
+  const obs = document.getElementById("text-value-obs").value;
 
-      pet: {
-        nome: document.getElementById("namePet").value,
-        idade: document.getElementById("idade").value,
-        porte: document.getElementById("porte").value,
-        pelagem: document.getElementById("pelagem").value,
-        raca: document.getElementById("raca").value,
-      },
-
-      servico: {
-        servicos: document.getElementById("services").value,
-      },
-
-      agendamento: {
-        horario: document.getElementById("dt-agenda").value,
-      },
-
-      observacao: {
-        observacao: document.getElementById("text-value-obs").value,
-      },
-    };
-    createClient(clientRequest);
-    clearFields();
-    alert("Criado novo agendamento!");
+  // Validar se todos os campos são preenchidos
+  if (
+    !nome ||
+    !email ||
+    !celular ||
+    !namePet ||
+    !pelagem ||
+    !raca ||
+    !idade ||
+    !porte ||
+    !services ||
+    !agenda
+  ) {
+    alert("Por favor, preencha todos os campos.");
+    return;
   }
-};
 
-function onClickButtonAgendar() {
-  sendRequest();
+  // Cria um objeto com os dados do cliente e manda para db_agenda.
+  const client = {
+    id: id,
+    nome: nome,
+    email: email,
+    celular: celular,
+    namePet: namePet,
+    idade: idade,
+    porte: porte,
+    pelagem: pelagem,
+    raca: raca,
+    services: services,
+    agenda: agenda,
+    obs: obs,
+    status: "Agendado", // Define o status como "Agendado"
+  };
+
+  // Salva a solicitação confirmada no local storage.
+  createClient(client);
+
+  // Limpa os campos do formulário.
+  clearFields();
+
+  // Exibe um alerta informando que a solicitação foi aceita.
+  alert("Criado novo agendamento!");
+  console.log("Lista atualizada: ", getLocalStorage());
 }
 
-const isValidFields = () => {
-  return document.getElementById("form").reportValidity();
-};
+function onClickButtonAgendar() {
+  sendClient();
+}
+
+const buttonAgendar = document.getElementById("agendar");
+buttonAgendar.addEventListener("click", onClickButtonAgendar);
 
 function setLocalStorage(db_agenda) {
   return localStorage.setItem("db_agenda", JSON.stringify(db_agenda));
@@ -69,7 +91,7 @@ function getLocalStorage() {
 
 function findClientIndexById(id) {
   const db_agenda = getLocalStorage();
-  return db_agenda.findIndex((client) => client.cliente.id === id);
+  return db_agenda.findIndex((client) => client.id === id);
 }
 
 // CRUD
@@ -84,10 +106,10 @@ function readClient() {
 }
 
 function updateClient(id, updatedClient) {
-  const db_agenda = getLocalStorage();
+  const db_agenda = readClient();
   const index = findClientIndexById(id);
   if (index !== -1) {
-    db_agenda[index] = updatedClient;
+    db_agenda[index] = { ...db_agenda[index], ...updatedClient };
     setLocalStorage(db_agenda);
   } else {
     console.error("Client not found");
@@ -95,7 +117,7 @@ function updateClient(id, updatedClient) {
 }
 
 function deleteClient(id) {
-  const db_agenda = getLocalStorage();
+  const db_agenda = readClient();
   const index = findClientIndexById(id);
   if (index !== -1) {
     db_agenda.splice(index, 1);
@@ -103,112 +125,4 @@ function deleteClient(id) {
   } else {
     console.error("Client not found");
   }
-}
-
-// DROPDOWN
-function dropDownIdade(parameterIdade) {
-  const elementsIdade = document.getElementsByClassName("dropDownIdade")[0];
-  if (parameterIdade === 0) {
-    elementsIdade.style.display = "block";
-    const translateIdade = ["0px", "0px, -10px"];
-    setTimeout(function () {
-      elementsIdade.style.transform =
-        "translate(" + translateIdade[parameterIdade] + ")";
-    }, 0);
-  } else {
-    elementsIdade.style.display = "none";
-  }
-}
-
-function idade(idade) {
-  const itemIdade = document.getElementById("itemIdade-" + idade).innerHTML;
-  document.getElementsByTagName("input")[5].value = itemIdade;
-}
-
-function dropDownPorte(parameterPorte) {
-  const elementsPorte = document.getElementsByClassName("dropDownPorte")[0];
-
-  if (parameterPorte === 0) {
-    elementsPorte.style.display = "block";
-
-    const translatePorte = ["0px", "0px, -10px"];
-    setTimeout(function () {
-      elementsPorte.style.transform =
-        "translate(" + translatePorte[parameterPorte] + ")";
-    }, 0);
-  } else {
-    elementsPorte.style.display = "none";
-  }
-}
-
-function porte(porte) {
-  const itemPorte = document.getElementById("itemPorte-" + porte).innerHTML;
-  document.getElementsByTagName("input")[6].value = itemPorte;
-}
-
-function dropDownPelagem(parameterPelagem) {
-  const elementsPelagem = document.getElementsByClassName("dropDownPelagem")[0];
-
-  if (parameterPelagem === 0) {
-    elementsPelagem.style.display = "block";
-
-    const translatePelagem = ["0px", "0px, -10px"];
-    setTimeout(function () {
-      elementsPelagem.style.transform =
-        "translate(" + translatePelagem[parameterPelagem] + ")";
-    }, 0);
-  } else {
-    elementsPelagem.style.display = "none";
-  }
-}
-
-function pelagem(pelagem) {
-  const itemPelagem = document.getElementById(
-    "itemPelagem-" + pelagem
-  ).innerHTML;
-  document.getElementsByTagName("input")[7].value = itemPelagem;
-}
-
-function dropDownRaca(parameterRaca) {
-  const elementsRaca = document.getElementsByClassName("dropDownRaca")[0];
-
-  if (parameterRaca === 0) {
-    elementsRaca.style.display = "block";
-
-    const translateRaca = ["0px", "0px, -10px"];
-    setTimeout(function () {
-      elementsRaca.style.transform =
-        "translate(" + translateRaca[parameterRaca] + ")";
-    }, 0);
-  } else {
-    elementsRaca.style.display = "none";
-  }
-}
-
-function raca(raca) {
-  const itemRaca = document.getElementById("itemRaca-" + raca).innerHTML;
-  document.getElementsByTagName("input")[8].value = itemRaca;
-}
-
-function dropDownServico(parameterServico) {
-  const elementsServico = document.getElementsByClassName("dropDownServico")[0];
-
-  if (parameterServico === 0) {
-    elementsServico.style.display = "block";
-
-    const translateServico = ["0px", "0px, -10px"];
-    setTimeout(function () {
-      elementsServico.style.transform =
-        "translate(" + translateServico[parameterServico] + ")";
-    }, 0);
-  } else {
-    elementsServico.style.display = "none";
-  }
-}
-
-function servico(servico) {
-  const itemServico = document.getElementById(
-    "itemServico-" + servico
-  ).innerHTML;
-  document.getElementsByTagName("input")[9].value = itemServico;
 }
