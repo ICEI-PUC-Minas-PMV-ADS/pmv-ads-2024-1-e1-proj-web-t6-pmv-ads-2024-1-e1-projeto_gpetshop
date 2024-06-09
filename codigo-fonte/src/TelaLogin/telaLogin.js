d86f9c5ee3cb0c9d843256a331d12e5b91454738
joaohleite <jhaleite@sga.pucminas.br>
2024-06-09 14:12:43 -0300

    apagando comentarios desnecessários

--- codigo-fonte/src/TelaLogin/telaLogin.js
+++ codigo-fonte/src/TelaLogin/telaLogin.js
@@ -3,15 +3,6 @@ import "./telaLogin.css";
 
 window.addEventListener('DOMContentLoaded', () => {
     onclickBtnLogar()
-    /* const a = [
-        {
-            usuario: "joao",
-            senha: "1234",
-            cargo: "Gerente"
-
-        }
-    ]
-    localStorage.setItem('listaColab', JSON.stringify(a)) */
 })
 
 function onclickBtnLogar() {
@@ -72,44 +63,3 @@ function logar() {
 
 
 
-/* function logar() {
-    var login = document.getElementById('user-field').value;
-    var senha = document.getElementById("password-field").value;
-
-    // Recupera a lista do localStorage
-    var listaColab = localStorage.getItem("listaColab");
-
-    if (listaColab) {
-        // Converte a string JSON em um array de objetos
-        listaColab = JSON.parse(listaColab);
-
-        // Percorre a lista de colaboradores e valida o login e a senha
-        let colaboradorEncontrado = false;
-
-        listaColab.forEach(item => {
-            if (login === item.usuario && senha === item.senha) {
-                colaboradorEncontrado = true;
-                //valida o cargo e direciona para a página específica
-                if (item.cargo === "Gerente") {
-                    window.location.href = "/codigo-fonte/dist/cadastroServicos/CadastroServicos.html";
-                } if (item.cargo === "Banhista") {
-                    location.href = "index.html";
-                } if (item.cargo === "Atendente") {
-                    location.href = "index.html";
-                }
-            }
-        });
-
-        if (!colaboradorEncontrado) {
-            alert("Login ou senha incorretos.");
-            // Limpa os campos de entrada
-            document.getElementById('user-field').value = '';
-            document.getElementById("password-field").value = '';
-        }
-    } else {
-        alert("Nenhuma lista encontrada no localStorage.");
-    }
-}
-
-let btnLogar = document.getElementById('Btn-logar');
-    btnLogar.addEventListener('click', logar) */
